4f1c5bae6371e11f6c5156ab1d4ccd3da82a84bc
Xu-Bin <bin.x.xu@qq.com>
2015-12-04 14:19:39 +0800

    Now can get weibo token

--- src/main/java/com/xubin/infofeed/MainActivity.java
+++ src/main/java/com/xubin/infofeed/MainActivity.java
@@ -11,9 +11,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.content.Context;
 
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
+import com.sina.weibo.sdk.auth.AuthInfo;
+import com.sina.weibo.sdk.auth.sso.SsoHandler;
+import com.sina.weibo.sdk.auth.WeiboAuthListener;
+import com.sina.weibo.sdk.auth.Oauth2AccessToken;
+import com.sina.weibo.sdk.exception.WeiboException;
+
 public class MainActivity extends FragmentActivity {
 
     public static final String TAG = "InfoFeed";
+
+    private AuthInfo mAuthInfo;
+    private Oauth2AccessToken mAccessToken;
+    private SsoHandler mSsoHandler;
 
     // private static boolean wifiConnected = false;
     // private static boolean mobileConnected = false;
@@ -30,6 +43,18 @@ public class MainActivity extends FragmentActivity {
         this.registerReceiver(receiver, filter);
 
         drawConnFragment();
+
+        /* ADD Weibo SSO login */
+        mAuthInfo = new AuthInfo(this, Constants.APP_KEY, Constants.REDIRECT_URL, Constants.SCOPE);
+        mSsoHandler = new SsoHandler(MainActivity.this, mAuthInfo);
+
+        findViewById(R.id.obtain_token_via_signature).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSsoHandler.authorize(new AuthListener());
+            }
+        });
+
     }
 
     protected void drawConnFragment(){
@@ -93,6 +118,65 @@ public class MainActivity extends FragmentActivity {
         public void onReceive(Context context, Intent intent){
             drawConnFragment();
             Log.i(TAG, "Entering onReceive");
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // SSO 授权回调
+        // 重要：发起 SSO 登陆的 Activity 必须重写 onActivityResults
+        if (mSsoHandler != null) {
+            mSsoHandler.authorizeCallBack(requestCode, resultCode, data);
+        }
+
+    }
+
+
+    class AuthListener implements WeiboAuthListener {
+
+        @Override
+        public void onComplete(Bundle values) {
+            // 从 Bundle 中解析 Token
+            mAccessToken = Oauth2AccessToken.parseAccessToken(values);
+            String  phoneNum =  mAccessToken.getPhoneNum();
+
+
+            if (mAccessToken.isSessionValid()) {
+                // 显示 Token
+                // updateTokenView(false);
+                //
+                // // 保存 Token 到 SharedPreferences
+                // AccessTokenKeeper.writeAccessToken(WBAuthActivity.this, mAccessToken);
+                // Toast.makeText(WBAuthActivity.this,
+                //         R.string.weibosdk_demo_toast_auth_success, Toast.LENGTH_SHORT).show();
+            } else {
+                // 以下几种情况，您会收到 Code：
+                // 1. 当您未在平台上注册的应用程序的包名与签名时；
+                // 2. 当您注册的应用程序包名与签名不正确时；
+                // 3. 当您在平台上注册的包名和签名与您当前测试的应用的包名和签名不匹配时。
+                String code = values.getString("code");
+                // String message = getString(R.string.weibosdk_demo_toast_auth_failed);
+                // if (!TextUtils.isEmpty(code)) {
+                //     message = message + "\nObtained the code: " + code;
+                // }
+                Toast.makeText(MainActivity.this, "code....", Toast.LENGTH_LONG).show();
+                Log.i(TAG, "code = " + code);
+            }
+        }
+
+        @Override
+        public void onCancel() {
+            // Toast.makeText(WBAuthActivity.this,
+            //        R.string.weibosdk_demo_toast_auth_canceled, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onWeiboException(WeiboException e) {
+             Toast.makeText(MainActivity.this,
+                     "Auth exception : " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 
